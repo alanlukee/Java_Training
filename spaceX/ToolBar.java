ab8165f3a4b50609efede8d7d1874b4803e651b9
Alan Luke <amenu4cse20205@am.students.amrita.edu>
2024-11-05 00:00:59 +0530

    Update ToolBar.java

--- spaceX/ToolBar.java
+++ spaceX/ToolBar.java
@@ -1,77 +1,77 @@
-package com.Exercise_1;
+package com.spaceX;
 
-import java.awt.FlowLayout;
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.BorderFactory;
-import javax.swing.JButton;
-import javax.swing.JPanel;
-
 public class ToolBar extends JPanel {
-	
-	
-	private JButton launchButton;
-	private JButton returnButton;
-	private JButton removeButton;
-	
-	private RocketListener rocketListener;
-	
-	
-	public ToolBar() {
-		
-		launchButton = new JButton("Launch Rocket");
-		returnButton = new JButton("Return to Starbase");
-		removeButton = new JButton("Remove rocket");
-		
-		setBorder(BorderFactory.createEtchedBorder());
-	
-		setLayout(new FlowLayout(FlowLayout.LEFT));
-		
-		launchButton.addActionListener(new ActionListener() {
-			
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				// TODO Auto-generated method stub
-				rocketListener.rocketEmitted();
-				
-				
-			}
-		});
-		
-		returnButton.addActionListener(new ActionListener() {
-			
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				// TODO Auto-generated method stub
-				rocketListener.rocketReturn();
-				
-			}
-		});
-		
-		removeButton.addActionListener(new ActionListener() {
-			
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				// TODO Auto-generated method stub
-				rocketListener.rocketCleared();
-				
-			}
-		});
-		
-		
-		add(launchButton);
-		add(returnButton);
-		add(removeButton);
+    
+    private JButton launchButton;
+    private JButton returnButton;
+    private JButton removeButton;
+    
+    private RocketListener rocketListener;
+    
+    public ToolBar() {
+        // Set background color to black
+        setBackground(Color.BLACK);
 
-		
-	}
+        launchButton = new JButton("Launch Rocket");
+        returnButton = new JButton("Return to Starbase");
+        removeButton = new JButton("Remove Rocket");
 
+        // Set button text color to white and background color to dark gray
+        launchButton.setForeground(Color.WHITE);
+        returnButton.setForeground(Color.WHITE);
+        removeButton.setForeground(Color.WHITE);
 
-	public void setRocketListener(RocketListener rocketListener) {
-		this.rocketListener = rocketListener;
-	}
-	
-	
+        launchButton.setBackground(Color.DARK_GRAY);
+        returnButton.setBackground(Color.DARK_GRAY);
+        removeButton.setBackground(Color.DARK_GRAY);
 
+        // Optional: Remove button border for a flat look
+        launchButton.setBorderPainted(false);
+        returnButton.setBorderPainted(false);
+        removeButton.setBorderPainted(false);
+
+        setBorder(BorderFactory.createEtchedBorder());
+        setLayout(new FlowLayout(FlowLayout.LEFT));
+
+        launchButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (rocketListener != null) {
+                    rocketListener.rocketEmitted();
+                }
+            }
+        });
+
+        returnButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (rocketListener != null) {
+                    rocketListener.rocketReturn();
+                }
+            }
+        });
+
+        removeButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (rocketListener != null) {
+                    rocketListener.rocketCleared();
+                }
+            }
+        });
+
+        add(launchButton);
+        add(returnButton);
+        //add(removeButton);
+    }
+
+    public void setRocketListener(RocketListener rocketListener) {
+        this.rocketListener = rocketListener;
+    }
 }
